c0809ebeb298b90c8c8cb5c156570cc78b4ca023
Vu Tung Lam <vutunglaminfo@gmail.com>
2025-10-10 15:52:01 +0700

    fix critical error in loading lgbm

--- main/src/lgbm.cpp
+++ main/src/lgbm.cpp
@@ -2,16 +2,16 @@
 #include "efeum/ml/lgbm.h"
 #include <iostream>
 
-int LoadLGBMModel(char const* modelFile, BoosterHandle* booster, int* numFeatures) {
+int LoadLGBMModel(char const* modelFile, BoosterHandle* pBooster, int* numFeatures) {
     int num_iterations;
-    int ret = LGBM_BoosterCreateFromModelfile(modelFile, &num_iterations, booster);
+    int ret = LGBM_BoosterCreateFromModelfile(modelFile, &num_iterations, pBooster);
     if (ret != 0) {
         std::cerr << "Failed to load model!" << std::endl;
         return ret;
     }
 
     if (numFeatures != NULL) {
-        LGBM_BoosterGetNumFeature(booster, numFeatures);
+        LGBM_BoosterGetNumFeature(*pBooster, numFeatures);
     }
 
     return ret;
